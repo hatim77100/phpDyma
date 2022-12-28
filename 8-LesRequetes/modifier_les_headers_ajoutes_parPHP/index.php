9c5f32568951dd57161aa4b5c9b4374151f2fa94
hatim77100 <najoua-hatim@hotmail.fr>
2022-12-28 13:56:41 +0100

    add requette http

---
+++ 8-LesRequetes/modifier_les_headers_ajoutes_parPHP/index.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "<pre>";
+
+// header('Content-type: text/plain');
+// print_r(headers_list());
+
+// supprimer un element dans le header
+
+header_remove('X-Powered-By');
+// header("Location: https://dyma.fr");
+
+
+echo "</pre>";
---
+++ 8-LesRequetes/modifier_les_headers_ajoutes_parPHP/req.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "<pre>";
+$a = 1;
+echo $GLOBALS['a'];
+print_r($_SERVER);
+
+
+echo "</pre>";
