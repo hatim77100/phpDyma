0a2f221233237773f5c86de57dedf8fbcf409b47
hatim77100 <najoua-hatim@hotmail.fr>
2022-12-28 19:16:52 +0100

    projet todo

--- projetTodo/index.php
+++ projetTodo/index.php
@@ -1,1 +1,23 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <?php require_once './includes/head.php' ?>
+    <title>Todo</title>
+</head>
+
+<body>
+    <div class="container">
+        <?php require_once './includes/header.php' ?>
+        <div class="content">
+            <div class="todo-container">
+                <h1>Ma Todo</h1>
+                <div class="todo-form"></div>
+                <div class="todo-list"></div>
+            </div>
+        </div>
+       <?php require_once './includes/footer.php' ?>
+    </div>
+</body>
+
+</html>
