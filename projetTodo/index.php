7b2ed298fc2ee39fd8a46322903e877ccd2289b1
hatim77100 <najoua-hatim@hotmail.fr>
2023-01-02 19:34:31 +0100

    todo updated

--- projetTodo/index.php
+++ projetTodo/index.php
@@ -1,3 +1,42 @@
+<?php
+const ERROR_REQUIRED = "Veuillez renseigner une todo";
+const ERROR_TOO_SHORT = "Veuillez entrer au moin 5 caractères";
+
+$filename = __DIR__ . "/data/todos.json";
+$error = '';
+$todo = '';
+$todos = [];
+
+if (file_exists($filename)) {
+    $data = file_get_contents($filename);
+    $todos = json_decode($data, true) ?? [];
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $todo = $_POST['todo'] ?? '';
+
+    if (!$todo) {
+        $error = ERROR_REQUIRED;
+    } else if (mb_strlen($todo) < 5) {
+        $error = ERROR_TOO_SHORT;
+    }
+
+    if (!$error) {
+        $todos = [...$todos, [
+            'name' => $todo,
+            'done' => false,
+            'id' => time(),
+        ]];
+        file_put_contents($filename, json_encode($todos));
+        header('Location: /');
+    }
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -12,11 +51,29 @@
         <div class="content">
             <div class="todo-container">
                 <h1>Ma Todo</h1>
-                <div class="todo-form"></div>
-                <div class="todo-list"></div>
+                <form action="/" method="POST" class="todo-form">
+                    <input value="<?= $todo ?>" name="todo" type="text">
+                    <button class="btn btn-primary">Ajouter</button>
+                </form>
+                <?php if ($error) : ?>
+                    <p class="text-danger"><?= $error ?></p>
+                <?php endif; ?>
+                <ul class="todo-list ">
+                    <?php foreach ($todos as $t) : ?>
+                        <li class="todo-item <?= $t['done'] ? 'low-opacity' : '' ?>">
+                            <span class="todo-name"><?= $t['name'] ?></span>
+                            <a href="/edit-todo.php?id=<?= $t['id'] ?>">
+                                <button class="btn btn-primary btn-small mr-2"><?= $t['done'] ? 'Annuler' : 'Valider' ?></button>
+                            </a>
+                            <a href="/remove-todo.php?id=<?= $t['id'] ?>">
+                                <button class="btn btn-danger btn-small ">Supprimer</button>
+                            </a>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
             </div>
         </div>
-       <?php require_once './includes/footer.php' ?>
+        <?php require_once './includes/footer.php' ?>
     </div>
 </body>
 
